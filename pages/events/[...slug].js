4bd05a8f1f2f3843b9aaef78c365f6294e73164d
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2021-09-24 16:35:44 +0300

    first commit

---
+++ pages/events/[...slug].js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class FilteredEventsPage extends Component {
+	render() {
+		return (
+			<div>
+				<h1>The filtered events page</h1>
+			</div>
+		);
+	}
+}
+
+export default FilteredEventsPage;
---
+++ pages/events/[id].js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class EventDetailPage extends Component {
+	render() {
+		return (
+			<div>
+				<h1>The event detail page</h1>
+			</div>
+		);
+	}
+}
+
+export default EventDetailPage;
