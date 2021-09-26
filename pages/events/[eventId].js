144d4a7303f979da50fc71b707fdeb5249b711d4
HasibullahMahmood <HasibullahMahmood@gmail.com>
2021-09-26 10:12:48 +0300

    event detail implemented

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,13 +1,29 @@
 import { Component } from 'react';
+import { withRouter } from 'next/router';
+
+import { getEventById } from '../../dummyData';
+import EventSummary from '../../components/pages/event-detail/event-summary';
+import EventLogistics from '../../components/pages/event-detail/event-logistics';
+import EventContent from '../../components/pages/event-detail/event-content';
 
 class EventDetailPage extends Component {
 	render() {
+		const { eventId } = this.props.router.query;
+		const event = getEventById(eventId);
+
+		if (!event) {
+			return <p>Found No Event!</p>;
+		}
 		return (
-			<div>
-				<h1>The event detail page</h1>
-			</div>
+			<>
+				<EventSummary title={event.title} />
+				<EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title} />
+				<EventContent>
+					<p>{event.description}</p>
+				</EventContent>
+			</>
 		);
 	}
 }
 
-export default EventDetailPage;
+export default withRouter(EventDetailPage);
